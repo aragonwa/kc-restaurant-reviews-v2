9dc7ff456434f98bde36c553dec3218c5da6fb78
Alex Aragon <Alex.Aragon@kingcounty.gov>
2017-06-20 13:22:42 -0700

    Adding react boostrap pkg for modal

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,14 +5,15 @@ import { getBusinessApi, getInspectionsApi } from '../api/api';
 import StringHelper from '../utils/StringHelper';
 import Ratings from '../utils/Ratings';
 // https://github.com/minhtranite/react-modal-bootstrap
-import {
-  Modal,
-  ModalHeader,
-  ModalTitle,
-  ModalClose,
-  ModalBody,
-  ModalFooter
-} from 'react-modal-bootstrap';
+// import {
+//   Modal,
+//   ModalHeader,
+//   ModalTitle,
+//   ModalClose,
+//   ModalBody,
+//   ModalFooter
+// } from 'react-modal-bootstrap';
+import {Modal} from 'react-bootstrap';
 import DetailsInspectionRow from './DetailsInspectionRow';
 
 class DetailsPage extends React.Component {
@@ -20,7 +21,8 @@ class DetailsPage extends React.Component {
     super(props, context);
     // State for Component
     this.state = {
-      isOpen: true,
+      // isOpen: true,
+      showModal: true,
       business: [],
       inspections: [],
       loading: true,
@@ -56,11 +58,11 @@ class DetailsPage extends React.Component {
   }
 
   openModal() {
-    this.setState({ isOpen: true });
+    this.setState({ showModal: true });
   }
 
   hideModal() {
-    this.setState({ isOpen: false });
+    this.setState({ showModal: false });
     this.context.router.push('/');
   }
 
@@ -73,7 +75,7 @@ class DetailsPage extends React.Component {
   }
   loadingModal() {
     return (
-      <Modal isOpen={this.state.isOpen} onRequestHide={this.hideModal} size={"modal-lg"}>
+      <Modal show={this.state.showModal} onHide={this.hideModal} bsSize="large">
         <div className="text-center">
           <span className="fa fa-spinner fa-spin fa-4x" />
         </div>
@@ -83,7 +85,7 @@ class DetailsPage extends React.Component {
 
   loadingModalError() {
     return (
-      <Modal isOpen={this.state.isOpen} onRequestHide={this.hideModal} size={"modal-lg"}>
+      <Modal show={this.state.showModal} onHide={this.hideModal} bsSize="large">
         <div className="col-sm-12">
           <div className="alert alert-danger"><h2>An error occurred while loading restaurant information.</h2></div>
         </div>
@@ -145,48 +147,88 @@ class DetailsPage extends React.Component {
     });
 
     let inspectionsRows = this.getInspectionRows(inspectionsLoading, inspections);
-
     return (
-      <Modal isOpen={isOpen} onRequestHide={this.hideModal} size={"modal-lg"}>
-        <ModalHeader>
-          <ModalClose onClick={this.hideModal} />
-          <ModalTitle>{business.businessName}{(business.businessProgramIdentifier) ? ', ' + business.businessProgramIdentifier : ''}</ModalTitle>
-        </ModalHeader>
-        <ModalBody>
-          <div className="row">
-            <div className="col-sm-6">
-              <div className="call-out-text call-out-text-primary m-t-0">
-                <div className="row">
-                  <div className="col-xs-6">
-                    <p>{StringHelper.capitalCase(business.businessAddress)} <br />
-                      {StringHelper.capitalCase(business.businessCity)}, WA {business.businessLocationZip}</p>
-                    <p className={(business.businessPhone) ? 'show' : 'hidden'}><span
-                      className="fa fa-phone" /> {StringHelper.phoneNumFormat(business.businessPhone)}</p>
-                  </div>
-                  <div className="col-xs-6 text-center">
-                    <p><img style={style} className="img-rounded" alt={rating.string} src={require('../assets/img/' + rating.img + '_70.gif')} /></p>
-                    <p>{rating.string}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-6">
-              <div className="call-out-text call-out-text-default m-t-0">
-                <p><span className="fa fa-color-danger fa-exclamation-circle" /> Critical violation</p>
-                <p><span className="fa fa-color-info fa-cog" /> Maintenance &amp; sanitation violation</p>
-                <p><a href="//www.kingcounty.gov/healthservices/health/ehs/foodsafety/inspections/system.aspx" target="_blank">Learn more about violations</a></p>
-              </div>
-            </div>
-          </div>
-          {inspectionsRows}
-        </ModalBody>
-        <ModalFooter>
-          <button className="btn btn-primary" onClick={this.hideModal}>
-            Close
-          </button>
-        </ModalFooter>
-      </Modal>
-    );
+        <Modal show={this.state.showModal} onHide={this.hideModal} bsSize="large">
+          <Modal.Header closeButton>
+            <Modal.Title>{business.businessName}{(business.businessProgramIdentifier) ? ', ' + business.businessProgramIdentifier : ''}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+                    <div className="row">
+             <div className="col-sm-6">
+               <div className="call-out-text call-out-text-primary m-t-0">
+                 <div className="row">
+                   <div className="col-xs-6">
+                     <p>{StringHelper.capitalCase(business.businessAddress)} <br />
+                       {StringHelper.capitalCase(business.businessCity)}, WA {business.businessLocationZip}</p>
+                     <p className={(business.businessPhone) ? 'show' : 'hidden'}><span
+                       className="fa fa-phone" /> {StringHelper.phoneNumFormat(business.businessPhone)}</p>
+                   </div>
+                   <div className="col-xs-6 text-center">
+                     <p><img style={style} className="img-rounded" alt={rating.string} src={require('../assets/img/' + rating.img + '_70.gif')} /></p>
+                     <p>{rating.string}</p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+             <div className="col-sm-6">
+               <div className="call-out-text call-out-text-default m-t-0">
+                 <p><span className="fa fa-color-danger fa-exclamation-circle" /> Critical violation</p>
+                 <p><span className="fa fa-color-info fa-cog" /> Maintenance &amp; sanitation violation</p>
+                 <p><a href="//www.kingcounty.gov/healthservices/health/ehs/foodsafety/inspections/system.aspx" target="_blank">Learn more about violations</a></p>
+               </div>
+             </div>
+           </div>
+           {inspectionsRows}
+          </Modal.Body>
+          <Modal.Footer>
+             <button className="btn btn-primary" onClick={this.hideModal}>
+             Close
+           </button>
+          </Modal.Footer>
+        </Modal>
+    )
+
+    // return (
+    //   <Modal isOpen={isOpen} onRequestHide={this.hideModal} size={"modal-lg"}>
+    //     <ModalHeader>
+    //       <ModalClose onClick={this.hideModal} />
+    //       <ModalTitle>{business.businessName}{(business.businessProgramIdentifier) ? ', ' + business.businessProgramIdentifier : ''}</ModalTitle>
+    //     </ModalHeader>
+    //     <ModalBody>
+    //       <div className="row">
+    //         <div className="col-sm-6">
+    //           <div className="call-out-text call-out-text-primary m-t-0">
+    //             <div className="row">
+    //               <div className="col-xs-6">
+    //                 <p>{StringHelper.capitalCase(business.businessAddress)} <br />
+    //                   {StringHelper.capitalCase(business.businessCity)}, WA {business.businessLocationZip}</p>
+    //                 <p className={(business.businessPhone) ? 'show' : 'hidden'}><span
+    //                   className="fa fa-phone" /> {StringHelper.phoneNumFormat(business.businessPhone)}</p>
+    //               </div>
+    //               <div className="col-xs-6 text-center">
+    //                 <p><img style={style} className="img-rounded" alt={rating.string} src={require('../assets/img/' + rating.img + '_70.gif')} /></p>
+    //                 <p>{rating.string}</p>
+    //               </div>
+    //             </div>
+    //           </div>
+    //         </div>
+    //         <div className="col-sm-6">
+    //           <div className="call-out-text call-out-text-default m-t-0">
+    //             <p><span className="fa fa-color-danger fa-exclamation-circle" /> Critical violation</p>
+    //             <p><span className="fa fa-color-info fa-cog" /> Maintenance &amp; sanitation violation</p>
+    //             <p><a href="//www.kingcounty.gov/healthservices/health/ehs/foodsafety/inspections/system.aspx" target="_blank">Learn more about violations</a></p>
+    //           </div>
+    //         </div>
+    //       </div>
+    //       {inspectionsRows}
+    //     </ModalBody>
+    //     <ModalFooter>
+    //       <button className="btn btn-primary" onClick={this.hideModal}>
+    //         Close
+    //       </button>
+    //     </ModalFooter>
+    //   </Modal>
+    // );
   }
 }
 
