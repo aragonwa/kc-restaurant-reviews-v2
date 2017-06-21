51e5d3ba43ef274e98b87adda89157253f4c4772
Alex Aragon <Alex.Aragon@kingcounty.gov>
2017-06-21 16:40:27 -0700

    helper to calc distance using lat/lng

---
+++ src/utils/Distance.js
@@ -0,0 +1,24 @@
+import distance from '@turf/distance';
+
+export default function(fromCords, toCords) {
+  const _format = 'miles'
+  const from = {
+    'type': 'Feature',
+    'properties': {},
+    'geometry': {
+      'type': 'Point',
+      'coordinates': fromCords
+    }
+  };
+  const to = {
+    'type': 'Feature',
+    'properties': {},
+    'geometry': {
+      'type': 'Point',
+      'coordinates': toCords
+    }
+  };
+
+
+  return distance(from, to, _format);
+};
